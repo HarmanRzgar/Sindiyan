922e4868e1ee6ebee9ec89944459458eb3b3b99b
Harman Rzgar <harmanrzgarmuhammad@gmail.com>
2025-02-11 22:21:07 +0300

    updated astro.config.mjs and made image fetching dynamic

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,17 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'static'
+  output: 'static',
+  vite: {
+    build: {
+      rollupOptions: {
+        external: ['/src/assets/logos/*.png']
+      }
+    },
+    resolve: {
+      alias: {
+        '@assets': '/src/assets'
+      }
+    }
+  }
 });
