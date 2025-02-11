29e5e216159b46af6575264f401e666062d08852
Harman Rzgar <harmanrzgarmuhammad@gmail.com>
2025-02-11 21:54:55 +0300

    corrected version for vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://vbartalis.github.io",
-  base: "/agency-template",
+  output: 'server',
+  adapter: vercel(),
+  image: {
+    remotePatterns: [{ protocol: "https" }],
+    service: { entrypoint: 'astro/assets/services/sharp' }
+  }
 });
